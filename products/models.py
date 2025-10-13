efe310c7dc4e619cde7931ca9c9e94d9bb9da040
j-mlo <joanna.r.davis@outlook.com>
2025-10-13 23:23:05 +0100

    Edit Product model for better product description display and additional images

--- products/models.py
+++ products/models.py
@@ -8,12 +8,16 @@ class Product(models.Model):
     )
 
     name = models.CharField(max_length=254)
+    subtitle = models.TextField(null=True, blank=True)
     description = models.TextField()
+    is_candle = models.BooleanField(default=True)
     size = models.CharField(choices=SIZE_CHOICES, default='180ml', null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    main_image = models.ImageField(upload_to='products/', null=True, blank=True)
+    extra_image1 = models.ImageField(upload_to='products/', null=True, blank=True)
+    extra_image2 = models.ImageField(upload_to='products/', null=True, blank=True)
     sku = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
