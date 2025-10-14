669ba1214121d710e14e31638456dde8c4b4316e
j-mlo <joanna.r.davis@outlook.com>
2025-10-14 14:37:11 +0100

    Add sort selector to products. BUG: sort selector doesn't sort products

--- products/views.py
+++ products/views.py
@@ -9,9 +9,11 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    sort = None
+    direction = None
 
-    # Query search
     if request.GET:
+        # Query search
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -20,11 +22,28 @@ def all_products(request):
             
             queries = Q(name__icontains=query) | Q(subtitle__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+            
+        # Product sorting
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+    
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
+    current_sorting = f'{sort}_{direction}' 
 
     context = {
         'products': products,
         'search_term': query,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
