057956279a91a6c41e7e2ba59b3c7520741b6aed
j-mlo <joanna.r.davis@outlook.com>
2025-10-18 15:52:22 +0100

    Apply migrations to checkout app models again

--- checkout/models.py
+++ checkout/models.py
@@ -15,7 +15,7 @@ class Order(models.Model):
     A model for generating an order.
     """
     order_number = models.CharField(max_length=32, null=False, editable=False)
-    # user = models.ForeignKey(UserProfile, on_delete=SET_NULL, null=True, blank=True, related_name='orders')
+    user = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=60, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
