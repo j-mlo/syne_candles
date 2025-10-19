a54a2cc5f445a4f2ffff8443f832f888b456a9a1
j-mlo <joanna.r.davis@outlook.com>
2025-10-19 16:03:34 +0100

    Add stripe elements, connect et xtra css to base.html

--- checkout/forms.py
+++ checkout/forms.py
@@ -29,18 +29,17 @@ class OrderForm(forms.ModelForm):
             'street_address2': 'Street Address 2', 
             'town_or_city': 'Town / City', 
             'postcode': 'Postal Code', 
+            'country': 'Country',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
 
         for field in self.fields:
-            if field != 'country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else: 
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-
-                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-                self.fields[field].label = False
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else: 
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
 
--- checkout/views.py
+++ checkout/views.py
@@ -9,10 +9,12 @@ def checkout(request):
         messages.error(request, "There's nothing in your basket at the moment")
         return redirect(reverse('products'))
     
-    order_form = OrderForm
+    order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51SBIWjFnNv9Vq3ftQJc2uFRQPP0fURiI206RWyGDQFXVdYVRkG5Lul1oZ1hpba8P4CT8dx8Xh7MoDILE2mSsi9Ds00btmaPVkY',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
