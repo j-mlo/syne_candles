9d92710a0acdd80f5493fa18478d0d6c87081476
j-mlo <joanna.r.davis@outlook.com>
2025-10-20 11:19:30 +0100

    Create checkout_success template and view and style in checkout.css

--- checkout/models.py
+++ checkout/models.py
@@ -41,8 +41,7 @@ class Order(models.Model):
         Update grand total each time a line item is added, 
         accounting for delivery costs. 
         """
-        self.order_total = self.lineitems.aggregate(
-            Sum('lineitem_total'))['lineitem_total_sum'] or 0
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         self.delivery_cost = Decimal('3.00')
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
