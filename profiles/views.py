0e4a604a1111674fe4d74f71ca8a3b6d2edea2a9
j-mlo <joanna.r.davis@outlook.com>
2025-10-21 14:19:37 +0100

    Add profiles view, urls, template and custom css file

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def profile(request):
+    """ Renders user's profile """
+
+    template = 'profiles/profile.html'
+    context = {
+
+    }
+
+    return render(request, template, context)
