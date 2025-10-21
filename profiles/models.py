9b14969f7d21f8feb1cb2392c624a50aa006b011
j-mlo <joanna.r.davis@outlook.com>
2025-10-22 00:17:01 +0100

    Add profiles app views, form and template

--- profiles/models.py
+++ profiles/models.py
@@ -26,5 +26,5 @@ class UserProfile(models.Model):
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """ Create or update the user profile"""
     if created:
-        UserProfile.object.create(user=instance)
+        UserProfile.objects.create(user=instance)
     instance.userprofile.save()
